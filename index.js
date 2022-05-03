cc36cfdfa93ff74cfd47588677215de88b2856a5
Veena Goud <veena.y@learnfinity.co>
2022-05-03 19:16:58 +0530

    Minor Change

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 function mathLibrary(options) {
   let { type, num1, num2 } = options;
-  let result = document.getElementById("#result");
+  const div = document.createElement("div");
 
   switch (type) {
     case "sum":
       console.log(num1 + num2);
-      return num1 + num2;
+      div.textContent = num1 + num2;
     case "diff":
       console.log(num1 - num2);
-      return num1 - num2;
+      div.textContent = num1 - num2;
     case "prod":
       console.log(num1 * num2);
-      return num1 * num2;
+      div.textContent = num1 * num2;
     case "divide":
       console.log(num1 / num2);
-      return num1 / num2;
+      div.textContent = num1 / num2;
     case "modulus":
       console.log(num1 % num2);
-      return num1 % num2;
+      div.textContent = num1 % num2;
     default:
-      return "Please provide correct details!";
+      div.textContent = "Please provide correct details!";
   }
+  document.body.appendChild(div);
 }
 module.exports.mathwizard = mathLibrary;
